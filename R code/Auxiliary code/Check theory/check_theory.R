1b47206177a3181f8f94e662d8e2373e4618903a
Maya Mathur <mmathur@stanford.edu>
2018-07-05 11:26:35 -0400

    Update sanity checks for new remark about sufficient conditions

--- R code/Auxiliary code/Check theory/check_theory.R
+++ R code/Auxiliary code/Check theory/check_theory.R
@@ -1,123 +1,76 @@
 
-# Goal: See if the convergence result seems to hold for OLS. 
+################## CHECK ALGEBRA IN REMARK ABOUT SUFFICIENT CONDITIONS ################## 
 
-N = 10000
+N = 100
 
 ##### Scenario 1: mean-centered ##### 
-# variables are mean-centered
-set.seed(451)
-varX = 3
-X = as.matrix( data.frame( int = rep(1, N), 
-                X1 = rnorm(N, mean=0, sd=sqrt(varX)) ) )
-              #  X2 = rnorm(N, mean=0, sd = 10) ) )
+varX = 2
 
-##### Scenario 2: not mean-centered ##### 
+int = rep(1,N)
+X1 = rnorm(N, mean=4, sd=sqrt(varX))
+X2 = X1 + rnorm(N, mean=0, sd = 1)  # correlated but not collinear
+#X2 = X1*3  # collinear
+sig2w = 1.2^2 # residual SE
 
-#set.seed(451)
-mean = 5
-varX = mean*3
-X = as.matrix( data.frame( int = rep(1, N), 
-                           X1 = rnorm( N, mean=mean, sd=sqrt(varX) ),
-                           X2 = rnorm(N, mean=mean, sd = 20) ) )
+cor(X1, X2)
+
+X = as.matrix( data.frame( int, X1, X2 ) )
+Y = rnorm( n=N, mean = X1 + X2, sd = sqrt(sig2w) )  # both betas = 1
+
+XXinv = solve( t(X) %*% X )
+
+m = lm( Y ~ X1 + X2 )
+summary(m)
+sig2hat = summary(m)$sigma^2
+V = vcov(m)
+
+# see correlations implied by this covariance matrix
+cov2cor(V)
+# indeed, the X1-X2 coefficient estimates are highly correlated
 
 
-t(X) %*% X
-S1
-S2
+######## Check Math ########
+
+# check the covariance-correlation relationship
+SEs = sqrt(diag(V))
+cov2cor(V)[2,] * SEs[2] * SEs; V[2,]
+# yes :) 
+
+# check the first line after "With the LHS in view"
+round( V, 3 ); round( XXinv * sig2w, 3 )  # should be very close but not equal because finite sample
+round( V, 3 ); round( XXinv * sig2hat, 3 ) # should be identical
+# yes :) 
+
+# next line (entry of (X'X)^{-1}X')
+n = 5  # arbitrary n
+LHS = XXinv %*% t(X)
+( LHS = LHS[2,n] )
+( RHS = (1/sig2hat) * V[2,] %*% t(X)[,n] )
+# yes :)
+
+# check the line above "Re-expressing"
+# we should get the correct asymptotic constant this way
+
+get_term = function(i, j) {
+  V[2,i] * V[2,j] * sum( X[,i] * X[,j] )
+}
+
+p = ncol(X)
+doublesum = 0
+for (i in 1:p) {
+  for (j in 1:p) {
+    doublesum = doublesum + get_term(i,j)
+  }
+}
+
+RHS = N * (1 / sig2hat^2) * doublesum
 
 A = solve( t(X) %*% X ) %*% t(X)
-dim(A)  # should be p X N
-
-# we're interested in the second regression coefficient (X1)
 Arow = A[2,]
 B = t(Arow)
-
-# sum of squared elements of B
 SSB = sum( B^2 )
-SSB * N
-# doesn't seem to blow up :) 
-
-
-
-##### Check Math - Version 2 ##### 
-
-### do I have the right expression for (X'X)?
-X1 = X[,2]
-
-S1 = sum(X1)
-S2 = sum(X1^2)
-
-# elements of matrix
-S1
-S2
-t(X) %*% X
-# yes :) 
-
-
-### do I have the right expression for (X'X)^{-1}?
-X1 = X[,2]
-
-c1 = 1 / ( N * S2 - S1^2 )
-
-# elements of matrix
-c1 * S2
-c1 * -S1
-c1 * N
-solve( t(X) %*% X )
-# yes :) 
-
-
-### do I have the right expression for A = (X'X)^{-1}?
-
-A = solve( t(X) %*% X ) %*% t(X) 
-
-# reproduce first column of A
-A[1,1]; c1 * ( S2 - X1[1]*S1 )
-A[2,1]; c1 * ( -S1 + X1[1]*N )
-# yes :) 
-
-
-### do I have the right expression for thing that's supposed to go to 0?
-# RHS of first line after "the regularity condition concerns the term"
-# vs. the next line
-
-c2 = 1 / ( N * S2^2 - 2 * S2 * S1^2 + (S1^4 / N) )
-#c2 = 1 / ( N * S2^2 - 2 * S2 * S1^2 + (S2 / N) )
-term = sum( ( (N * X1) - S1 )^2 )
-
-N * sum( A[2,]^2 ); c2 * term
-# yes :) 
-# ~~~~~ DOESN'T WORK IF NOT MEAN-CENTERED
-
-
-### keep simplifying the sum term
-# pull sum through
-( N^2 * S2 - 2 * N * S1^2 + N*S1^2 ) * c2
-
-# penultimate
-(N^2 * S2 - N*S1^2 ) / (N * S2^2 - 2 * S2*S1^2 + (S1^4)/N )
-# still matches!! :D
-
-# last line
-num = (S2/N - S1^2/(N^2) )
-denom =  ( S2^2/(N^2) - 2 * (S2/N) * S1^2/(N^2) + (S1^4)/(N^4) )
-num/denom
-# still matches!! :D
-
-
-# should approach this as n -> infty
-1/varX
-
-
-### understand what happens with mean-centering
-
-# if Var(X) = (E(X))^2, then denom goes to 0 becasue:
-( E.X2 = varX + mean^2 )
-E.X2^2 - 2 * E.X2 * mean^2
-# this is zero
-
-
-
-
+LHS = SSB * N
+RHS; LHS
+# yessss :) 
 
 
